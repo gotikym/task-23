fb59400921458741d5ec4afd63d869a902939f32
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2022-05-28 21:47:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,42 +1,39 @@
-﻿using System;
+using System;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        int [] array = new int [30];
-        Random rand = new Random();
+        byte minRandomNumber = 0;
+        byte maxRandomNumber = 26;
+        byte elements = 30;
+        int[] array = new int[elements];
+        Random random = new Random();
 
         for (int i = 0; i < array.Length; i++)
         {
-            array[i] = rand.Next(0,25);
+            array[i] = random.Next(minRandomNumber, maxRandomNumber);
             Console.Write(array[i] + " ");
         }
 
         Console.WriteLine("\n Локальные максимумы:");
-        
-        for (int i = 0; i < array.Length; i++)
+
+        if (array[0] > array[1])
         {
-            if (i == 0)
-            {
-                if (array[i] > array[i+1])
-                {
-                    Console.Write(array[i] + " ");
-                }
-            }
+            Console.Write(array[0] + " ");
+        }
 
-            else if (i == array.Length - 1)
-            {
-                if (array[i] > array[i - 1])
-                {
-                    Console.Write(array[i] + " ");
-                }
-            }
-
-            else if (array[i - 1] < array[i] && array[i] > array [i + 1] )
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            if (array[i - 1] < array[i] && array[i] > array[i + 1])
             {
                 Console.Write(array[i] + " ");
             }
+        }
+
+        if (array[elements - 1] > array[elements - 2])
+        {
+            Console.Write(array[elements - 1] + " ");
         }
     }
 }
